6b5ef0e37f9546b0f41e8cf1cba6328f3e90eb31
duru <enfn2001@gmail.com>
2016-07-12 10:53:25 +0900

    실습-과제다시하기~

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,26 @@
+package next.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.db.DataBase;
+import next.model.User;
+
+@WebServlet("/user/profile")
+public class UpdateUserFormServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      
+    }
+}
